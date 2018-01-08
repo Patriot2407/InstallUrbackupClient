8fa1916a4f616432e9b39c2e56f2842d2987da78
czebiniak <30630518+czebiniak@users.noreply.github.com>
2018-01-08 15:31:11 -0500

    Ver 1.0
    minor

--- install_urbackup_client.sh
+++ install_urbackup_client.sh
@@ -1,22 +1,15 @@
 #!/bin/bash
+# VARS
+VER='2.1.17'
+# PREREQS
 sudo apt-get install build-essential "g++" libwxgtk3.0-dev "libcrypto++-dev" libz-dev -y
-wget https://hndl.urbackup.org/Client/2.1.17/urbackup-client-2.1.17.tar.gz
-tar xzf urbackup-client-2.1.17.tar.gz
-cd urbackup-client-2.1.17
-./configure
+wget https://hndl.urbackup.org/Client/$VER/urbackup-client-$VER.tar.gz
+tar xzf urbackup-client-$VER.tar.gz
+# build
+cd urbackup-client-$VER
+./configure --enable-headless
 make -j4
 sudo make install
-sudo urbackupclientbackend -v info
-sudo echo "[Unit]
-Description=URBackup client
-[Service]
-Type=simple
-Restart=on-failure
-RestartSec=5
-ExecStart=/usr/local/sbin/urbackupclientbackend -d
-ExecStop=killall -TERM srcds_linux
-[Install]
-WantedBy=multi-user.target" >> /etc/systemd/system/urbackupclient.service
-sudo systemctl start urbackupclient
-sudo systemctl status urbackupclient
+# test and run
+sudo urbackupclientbackend -i -v debug
 exit 0
