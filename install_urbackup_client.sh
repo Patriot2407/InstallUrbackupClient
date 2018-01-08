2e99d85936f69241873a59479059867a7b5a1748
czebiniak <30630518+czebiniak@users.noreply.github.com>
2018-01-08 16:05:40 -0500

    Ver 1.0
    fixes

--- install_urbackup_client.sh
+++ install_urbackup_client.sh
@@ -14,15 +14,9 @@ sudo make install
 sudo urbackupclientbackend -v debug
 sudo echo "
 [Unit]
-Description=ARK Survival Evolved
+Description=urbackup client headless
 [Service]
-Type=simple
-Restart=on-failure
-RestartSec=5
-StartLimitInterval=60s
-StartLimitBurst=3
-ExecStart=/Ark/ShooterGame/Binaries/Linux/ShooterGameServer Ragnarok?listen?SessionName=ArkWorld -nosteamclient -server -log
-ExecStop=killall -TERM srcds_linux
+ExecStart=/usr/local/sbin/urbackupclientbackend -v info
 [Install]
 WantedBy=multi-user.target
 " >> /etc/systemd/system/urbackup.service
